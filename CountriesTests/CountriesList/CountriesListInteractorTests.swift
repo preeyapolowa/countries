53300a332d9888f717cd0d55edfcabeda070df57
Preeyapol Owatsuwan <preeyapol.owa@gmail.com>
2022-08-25 23:40:34 +0700

    Create unit tests - create CountriesListInteractorTests, CountriesListPresenterTests

---
+++ CountriesTests/CountriesList/CountriesListInteractorTests.swift
@@ -0,0 +1,19 @@
+//
+//  CountriesTests.swift
+//  CountriesTests
+//
+//  Created by Preeyapol Owatsuwan on 24/8/2565 BE.
+//
+
+import XCTest
+@testable import Countries
+
+class CountriesTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
---
+++ CountriesTests/CountriesList/CountriesListPresenterTests.swift
@@ -0,0 +1,19 @@
+//
+//  CountriesListPresenterTests.swift
+//  CountriesTests
+//
+//  Created by Preeyapol Owatsuwan on 25/8/2565 BE.
+//
+
+import XCTest
+@testable import Countries
+
+class CountriesListPresenterTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
